d4148727681949fdcd8f823ca8d080b1884d3b76
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-09-01 22:47:48 +0530

    regular comp

---
+++ NEW_COMPONENTS/PureComponent/RegComp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class RegComp extends Component {
+  render() {
+    console.log("Regular component");
+    return (
+        <>
+      <div>
+        Regular Component
+      </div>
+      </>
+    )
+  }
+}
+
+export default RegComp
