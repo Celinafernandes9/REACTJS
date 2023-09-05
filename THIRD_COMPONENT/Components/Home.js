1b83ed6e465c688634a24def3189713bf980aee0
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-09-05 23:44:44 +0530

    routing

--- THIRD_COMPONENT/Components/Home.js
+++ THIRD_COMPONENT/Components/Home.js
@@ -14,7 +14,7 @@ export const Home = () => {
         NEW COLLECTION<br></br>SALE 40% 
       </h4>
       
-    <button type="button" className="btn btn-danger text-danger pl-5 m-4 bg-transparent" onClick={()=>navigate('collection')}>SHOP NOW</button>
+    <button type="button" className="btn btn-danger text-danger bg-transparent" onClick={()=>navigate('collection')}>SHOP NOW</button>
     </section>
     </>
   )
