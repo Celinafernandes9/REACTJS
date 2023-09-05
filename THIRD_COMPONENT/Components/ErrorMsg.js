83a062a73bd6199687d6661f594cac6bca41de89
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-09-05 23:44:23 +0530

    routing

--- THIRD_COMPONENT/Components/ErrorMsg.js
+++ THIRD_COMPONENT/Components/ErrorMsg.js
@@ -4,9 +4,9 @@ export const ErrorMsg = () => {
   return (
     <>
     <div className='bg-black'>
-    <div className='text-center text-white'>404</div>
-    <div className='text-center text-white'>Page Not Found!</div>
-    <div className='text-center text-white'>The requested page was not found</div>
+    <div className='text-center text-white fs-3'>404</div>
+    <div className='text-center text-white fs-3'>Page Not Found!</div>
+    <div className='text-center text-white fs-3'>The requested page was not found</div>
     </div>
     </>
 
