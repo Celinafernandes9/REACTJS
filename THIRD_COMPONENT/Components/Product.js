3f84249dd665ce0ca7d0cf1e901e9761d99bdcd3
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-09-06 23:17:29 +0530

    nested routing

--- THIRD_COMPONENT/Components/Product.js
+++ THIRD_COMPONENT/Components/Product.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import b1 from "../Image/b1.jpg";
 import m1 from "../Image/m1.jpg";
 import f1 from "../Image/f1.jpg";
@@ -8,23 +8,25 @@ export const Product = () => {
   return (
     <>
       <div className="content mb-4 mt-3 m-5 fw-bold fs-2">Product</div>
-      <input type="search" placeholder="Search product" className="m-4" />
+      <input type="search" placeholder="Search product" className="m-4 " />
 
       <nav className="nav">
-        <Link to="/men" className="m-4 text-decoration-none text-black">
+        <Link to="men" className="m-4 text-decoration-none text-black ">
           <img src={m1} alt=" " height={70} width={70} className="rounded-circle m-3" />
           Men
         </Link>
 
-        <Link to="/women" className="m-4 text-decoration-none text-black">
+        <Link to="women" className="m-4 text-decoration-none text-black">
           <img src={f1} alt=" " height={70} width={70} className="rounded-circle m-3" />
           Women
         </Link>
       </nav>
 
       <section className="banner">
-        <img src={b1} alt="banner" height={250} className="w-100" />
+        <img src={b1} alt="banner" height={200} className="w-100" />
       </section>
+
+      <Outlet/>
     </>
   );
 };
