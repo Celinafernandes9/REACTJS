267d662d98de8362c820a84738c3a5e0356ecc2c
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-09-06 23:21:13 +0530

    routing

--- THIRD_COMPONENT/Components/Men.js
+++ THIRD_COMPONENT/Components/Men.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import s1 from '../Image/s1.jpg'
 import s2 from '../Image/s2.jpg'
 import s3 from '../Image/s3.jpg'
@@ -9,20 +8,20 @@ import s4 from '../Image/s4.jpg'
 export const Men = () => {
   return (
     <>
-    <div>Men</div>
     <h1 className="text-center">Men's Clothing</h1>
-    <div>
-      <img src={s1} alt=''/>
-    </div>
-    <div>
-      <img src={s2} alt=''/>
-    </div>
-    <div>
-      <img src={s3} alt=''/>
-    </div>
-    <div>
-      <img src={s4} alt=''/>
-    </div>
+    <div className="pro-container d-flex flex-wrap"></div>
+        <div className="border border-dark m-3">
+          <img src={s1} alt="" />
+        </div>
+        <div className="border border-dark m-3">
+          <img src={s2} alt="" />
+        </div>
+        <div className="border border-dark m-3">
+          <img src={s3} alt="" />
+        </div>
+        <div className="border border-dark m-3">
+          <img src={s4} alt="" />
+        </div>
     </>
   )
 }
