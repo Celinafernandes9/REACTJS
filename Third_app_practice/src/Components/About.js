81780029f04081428d66e9fad5d9b72f52a08874
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-09-25 22:40:52 +0530

    About page

---
+++ Third_app_practice/src/Components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <>
+    <div className='text-center fw-bold fs-2'>About</div>
+    <div className='m-3'>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla magna tellus, non dictum arcu tempor non. Curabitur tempus varius nulla sit amet accumsan. Vestibulum velit quam, rhoncus sit amet pretium vel, elementum a lorem. Sed ipsum tortor, lacinia eu nisl, varius pellentesque ipsum. Fusce pulvinar, mauris quis vehicula fermentum, massa mauris lobortis quam, vel volutpat dolor massa nec justo. Pellentesque venenatis nulla vitae libero convallis aliquet. Integer eu dapibus ex. Maecenas mauris ipsum, tempus vel volutpat vitae, pharetra a mi.
+    </div>
+    </>
+  )
+}
+
