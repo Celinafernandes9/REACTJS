922df768f87240b96a8a08ad3f933ce98964b6f2
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-09-26 10:04:19 +0530

    main file

---
+++ Third_app_practice/src/App.js
@@ -0,0 +1,40 @@
+
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import {Home} from './Components/Home';
+import {About} from './Components/About';
+import {Navbar} from './Components/Navbar';
+import { Order } from './Components/Order';
+import { ErrorMsg } from './Components/ErrorMsg';
+import { Product } from './Components/Product';
+import { Collection } from './Components/Collection';
+import { Men } from './Components/Men';
+import { Women } from './Components/Women';
+
+function App() {
+  return (
+    <>
+    
+    <Navbar/>
+
+    <Routes>
+
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/order' element={<Order/>}></Route>
+      <Route path='/collection' element={<Collection/>}></Route>
+      <Route path='*' element={<ErrorMsg/>}></Route>
+      
+      <Route path='/product' element={<Product/>}>
+        <Route path='men' element={<Men/>}/>
+
+        <Route path='women' element={<Women/>}/>
+      </Route>
+
+    </Routes>
+    </>
+  );
+}
+
+export default App;
