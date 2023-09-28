aed38f4f8df2cd5c21cf716de36796e200d28138
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-09-28 23:44:41 +0530

    function component

---
+++ first_app_practice/src/components/greet.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function greet(props) {
+  return (
+    <>
+    <div>
+      hello welcome to reactjs 
+    </div>
+    <h1>
+      hello my name is {props.name}
+    </h1>
+    </>
+  )
+}    
+
+export default greet
