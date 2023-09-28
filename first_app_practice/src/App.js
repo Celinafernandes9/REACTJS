b042f673bdc8d4600d62c46f180b159f7d85e64e
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-09-28 22:27:00 +0530

    main file

---
+++ first_app_practice/src/App.js
@@ -0,0 +1,36 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Conditionalrender from './components/Conditional/Conditionalrender';
+// import Parentcomp from './components/parent_child_comp/Parentcomp';
+
+// import Countclick from './components/Countclick';
+// import Greet from './components/greet';
+// import Welcome from './components/welcome';
+// import Change from './components/Change';
+
+// import Events from './components/Event_handling/Events';
+import Array from './components/List_rendering/Array';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      
+      {/* <Change/>  */}
+     {/* <Countclick/> */}
+      {/* <Greet name="abc"/>
+      <Welcome name="abc2" city="wadi"/> */}
+
+      {/* <Events/> */}
+
+      {/* <Parentcomp/> */}
+      {/* <Conditionalrender/> */}
+      <Array/>
+   
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
