28dcaedca76e18d5269be314fd3e650c5a79619c
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-09-28 22:18:47 +0530

    main file

---
+++ second_app_practice/src/App.js
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Count from './AxiosData/Hooks/Count';
+// import Form from './AxiosData/Form';
+// import PostForm from './AxiosData/PostForm';
+// import Memo from './Components/MemoComponent/memo';
+// import ParentComp from './Components/PureComponent/ParentComp';
+
+// import LifeCycleMethod from './LifeCycle/LifeCycleMethod';
+// import Form from './Styles/Form/Form';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Form/> */}
+
+     {/* <LifeCycleMethod/> */}
+
+    {/* <ParentComp/> */}
+    {/* <Memo/> */}
+
+    {/* <PostForm/> */}
+    {/* <Form/> */}
+
+    {/* <Count/> */}
+    </div>
+  );
+}
+
+export default App;
