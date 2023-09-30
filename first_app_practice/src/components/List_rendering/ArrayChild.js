7ad4d219542f2e519c3b077ff5b25804ed3ff723
Celinafernandes9 <celina.922.fernz@gmail.com>
2023-09-30 21:09:14 +0530

    using props

---
+++ first_app_practice/src/components/List_rendering/ArrayChild.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ArrayChild({list}) {
+  return (
+    <>
+     <div></div>
+      <h2>
+        Hii ... My name is {list.name} my id no is {list.id} and my age is {list.age}
+      </h2>
+    
+    </>
+  )
+}
+
+export default ArrayChild
